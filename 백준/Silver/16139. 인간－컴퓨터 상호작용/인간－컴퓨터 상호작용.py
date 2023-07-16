82083c694e6dd663f62ddcc23286055705f2e97d
jae <121669713+jaekwanyda@users.noreply.github.com>
2023-07-17 01:11:00 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 104 ms, Memory: 34140 KB -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -7,12 +7,23 @@ import copy
 s = input()
 s_list = list(s)
 #각 alphabet 개수를 저장해주자
-s_dict_list = [defaultdict(int) for _ in range(len(s)+1)]
+s_dict_list = [{} for _ in range(len(s)+1)]
 for i,j in enumerate(s_list):
-    s_dict_list[i] = copy.deepcopy(s_dict_list[i-1])
-    s_dict_list[i][j] += 1
+    s_dict_list[i+1] = copy.deepcopy(s_dict_list[i])
+    if j in s_dict_list[i+1]:
+        s_dict_list[i+1][j] += 1
+    else:
+        s_dict_list[i+1][j] = 1
 #n 입력받기
 n = int(input())
 for _ in range(n):
     find_s, start, end = input().split()
-    print(s_dict_list[int(end)][find_s]-s_dict_list[int(start)-1][find_s])
+    if find_s in s_dict_list[int(end)+1]:
+        a = s_dict_list[int(end)+1][find_s]
+    else:
+        a=0
+    if find_s in s_dict_list[int(start)]:
+        b = s_dict_list[int(start)][find_s]
+    else:
+        b=0
+    print(a-b)
