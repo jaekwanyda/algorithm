51d92be88882c763582e2bf0ac96cc9a864eea3f
jae <121669713+jaekwanyda@users.noreply.github.com>
2023-09-12 15:30:52 +0900

    [Silver I] Title: 링크와 스타트, Time: 1892 ms, Memory: 116232 KB -BaekjoonHub

---
+++ 백준/Silver/15661. 링크와 스타트/링크와 스타트.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+#아이디어: 조합 + 전체 시너지 합을 미리 구하면 시간을 좀 단축할 수 있을 것 같다
+from itertools import combinations
+#n입력받기
+n = int(input())
+#시너지 입력받기
+array = [list(map(int,input().split())) for _ in range(n)]
+total = sum(sum(a) for a in array)
+min_val = 99999999
+for i in range(1,n//2+1):
+    for team1 in combinations(range(n),i):
+        team = set(range(n))
+        val1 = 0
+        for x in team1:
+            for y in team1:
+                val1 += array[x][y]
+        team2 = team - set(team1)
+        val2 = 0
+        for x in team2:
+            for y in team2:
+                val2 += array[x][y]
+        val = abs(val1-val2)
+        min_val = min(min_val,val)
+print(min_val)
